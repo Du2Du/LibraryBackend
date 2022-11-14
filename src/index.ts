95de9df387f10caeec01451e4c9d3a31396fb56e
Du2Du <eduardohilario.eha@gmail.com>
2022-11-13 22:37:00 -0300

    Tentando inicializar banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import Fastify from "fastify";
+import Fastify, { FastifyRequest } from "fastify";
 import * as dotenv from "dotenv";
 import fastifyPg from "@fastify/postgres";
 import fastifyCors from "@fastify/cors";
+import { Client } from "pg";
 
 dotenv.config();
 
+const client = new Client({
+  user: "postgres",
+  password: process.env.PASSWORD,
+  host: "localhost",
+  port: 5432,
+  database: process.env.DATABASE_URL,
+});
 const app = async () => {
   const fastify = Fastify({
     logger: true,
@@ -14,8 +22,19 @@ const app = async () => {
     origin: true,
   });
 
-  fastify.register(fastifyPg, {
-    connectionString: process.env.DATABASE_URL,
+  fastify.register(fastifyPg, client);
+
+  fastify.post("/livro", (req: FastifyRequest, res) => {
+    fastify.pg.query(
+      "CREATE TABLE IF NOT EXISTS BOOK ( id INTEGER PRIMARY KEY, name VARCHAR)",
+      [req.body.name],
+      (err, result) => {
+        res.send({
+          result,
+          err,
+        });
+      }
+    );
   });
 
   fastify.listen({ port: 8099 });
