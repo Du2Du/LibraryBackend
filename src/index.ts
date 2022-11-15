68db6f45c0c34d732fb134229ad17b69cc4428e3
Du2Du <eduardohilario.eha@gmail.com>
2022-11-15 20:04:11 -0300

    Retirando biblioteca

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import Fastify, { FastifyRequest } from "fastify";
 import * as dotenv from "dotenv";
-import fastifyPg from "@fastify/postgres";
 import fastifyCors from "@fastify/cors";
-import { Client } from "pg";
 
 dotenv.config();
-
-const client = new Client({
-  user: "postgres",
-  password: process.env.PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: process.env.DATABASE_URL,
-});
 const app = async () => {
   const fastify = Fastify({
     logger: true,
@@ -20,21 +10,6 @@ const app = async () => {
 
   await fastify.register(fastifyCors, {
     origin: true,
-  });
-
-  fastify.register(fastifyPg, client);
-
-  fastify.post("/livro", (req: FastifyRequest, res) => {
-    fastify.pg.query(
-      "CREATE TABLE IF NOT EXISTS BOOK ( id INTEGER PRIMARY KEY, name VARCHAR)",
-      [req.body.name],
-      (err, result) => {
-        res.send({
-          result,
-          err,
-        });
-      }
-    );
   });
 
   fastify.listen({ port: 8099 });
