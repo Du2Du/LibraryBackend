b9546468ce8778c89a1405a2d513a91eac27f9af
Du2Du <eduardohilario.eha@gmail.com>
2022-11-17 15:44:47 -0300

    Melhorando metodo de criar livro

--- src/Model/BOs/BookBO/index.ts
+++ src/Model/BOs/BookBO/index.ts
@@ -1,9 +1,34 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { ConflictError } from "http-errors-enhanced";
+import { bookDAO } from "../../DAOs";
 import { createBookSchema } from "../../DTOs";
+import { UserBO } from "../UserBO";
 
 export const BookBO = () => {
+  const { me } = UserBO();
+
+  const findBookFromNameWithSallerId = async (
+    name: string,
+    sallerId: number
+  ) => {
+    const book = await bookDAO.findFirst({
+      where: {
+        bookName: name,
+        sallerId: sallerId,
+      },
+    });
+    return book;
+  };
+
   const createBook = async (req: FastifyRequest, res: FastifyReply) => {
     const createBookData = createBookSchema.parse(req.body);
+    const { bookName } = createBookData;
+    const currentUser = await me(req, res);
+
+    if (await findBookFromNameWithSallerId(bookName, currentUser.id))
+      throw new ConflictError(
+        "Livro já cadastrado, a melhor opção seria aumentar a quantidade."
+      );
   };
 
   return {
